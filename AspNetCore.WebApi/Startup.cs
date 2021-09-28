0b3ca99c1c5f61b51d37ebb23aa196fc327c44d7
Moacir Silva <moacirsilva.it@gmail.com>
2021-09-28 16:21:44 -0300

    Configure Swagger, reafactor startup.cs, create extensions methods

--- AspNetCore.WebApi/Startup.cs
+++ AspNetCore.WebApi/Startup.cs
@@ -1,19 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ElmahCore.Mvc;
-using ElmahCore.Sql;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using AspNetCore.WebApi.Extensions;
 
 namespace AspNetCore.WebApi
 {
@@ -23,56 +13,26 @@ namespace AspNetCore.WebApi
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
-
-            services.AddElmah<SqlErrorLog>(options =>
-            {
-                options.ConnectionString = Configuration.GetSection($"ConnectionStrings:ElmahConnection").Value;
-                options.Path = "/elmah";
-            });
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AspNetCore.WebApi", Version = "v1" });
-            });
+            services.AddElmahCore(Configuration);
+            services.AddSwagger();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspNetCore.WebApi v1"));
-            }
+                app.UseDeveloperExceptionPage(); 
 
             app.UseHttpsRedirection();
-
+            app.UseElmahCore();
             app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseWhen(context => context.Request.Path.StartsWithSegments("/elmah", StringComparison.OrdinalIgnoreCase), appBuilder =>
-            {
-                appBuilder.Use(next =>
-                {
-                    return async ctx =>
-                    {
-                        ctx.Features.Get<IHttpBodyControlFeature>().AllowSynchronousIO = true;
-                        await next(ctx);
-                    };
-                });
-            });
-
-            app.UseElmah();
+            app.UseAuthorization(); 
+            app.UseSwaggerUI();
 
             app.UseEndpoints(endpoints =>
             {
