5f4e23b76527979e8c96d8d3d33cfe2acd008b8b
Moacir Silva <moacirsilva.it@gmail.com>
2021-09-30 17:27:06 -0300

    add get livros

--- AspNetCore.WebApi/Controllers/LivroController.cs
+++ AspNetCore.WebApi/Controllers/LivroController.cs
@@ -5,8 +5,29 @@ using Microsoft.AspNetCore.Mvc;
 namespace AspNetCore.WebApi.Controllers
 {
     [ApiController]
+    [Route("api/")]
     public class LivroController : ControllerBase
     {
+        [HttpGet("livro-nao-tipado")]
+        public IActionResult ObterTodosNaoTipado()
+        {
+            var livros = Livro.ObterLivros();
+            return Ok(livros);
+        }
+        [HttpGet("livro-tipado")]
+        public IActionResult ObterTodosTipado()
+        {
+            var livros = Livro.ObterLivros();
+            return Ok(livros);
+        }
+        /// <sumary>
+        /// Obtém todos os livros
+        /// </sumary>
+        /// <returns> Retorna os livros encontrados </returns>
+        /// <response code="200">Retorna os livros tipados</response>
+        [ProducesResponseTypeAttribute(StatusCodes.Status200k)];
+        [ProducesResponseTypeAttribute(StatusCodes.Status400BadRequest)]
+        [HttpGet("livro-nao-tipado")]
         public IActionResult ObterTodosNaoTipado()
         {
             var livros = Livro.ObterLivros();
